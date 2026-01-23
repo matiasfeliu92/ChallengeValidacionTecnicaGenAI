b974cc37ab46a2e108dbb3ab4e0f6903e12bd737
matiasfeliu92 <matumazparrote@gmail.com>
2026-01-23 17:34:07 -0300

    delete unnecesary models

--- technical_challenge/models/gold/gold_daily_metrics.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-select
-    date_trunc('day', event_timestamp) as report_date,
-    count(distinct user_id) as daily_active_users,
-    count(distinct case when event_type = 'user_login' then event_id end) as total_logins,
-    count(distinct case when event_type = 'document_edit' then event_id end) as total_edits
-from {{ ref('stage_event_logs') }}
-group by 1
