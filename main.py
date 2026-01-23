1b1fb501e00fa081f4fa8321fdef9199834f6978
matiasfeliu92 <matumazparrote@gmail.com>
2026-01-23 17:34:31 -0300

    delete unnecesary main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from src.scripts.load_data import LoadData
-from src.scripts.extract_data import ExtractData
-
-if __name__ == "__main__":
-    extract_data = ExtractData()
-    data_extracted = extract_data.extract()
-    load_data = LoadData()
-    load_data.load(data_extracted)
